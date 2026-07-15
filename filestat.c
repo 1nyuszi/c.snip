1ec16d445b47c195a8b1f13b3bbaff2b576052e7
1nyuszi <pal.balazs2@gmail.com>
2026-07-15 21:25:56 +0200

    Start

---
+++ filestat.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int ct_lines = 0;
+    int ct_chars = 0;
+
+    printf("Number of lines in the file: %d\n", ct_lines);
+    printf("Number of characters in the file: %d\n", ct_chars);
+
+    return 0;
+}
