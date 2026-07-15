01385bfba426c9f9cba70c74920dd5ed00c4c9be
1nyuszi <pal.balazs2@gmail.com>
2026-07-15 21:49:14 +0200

    Styling compile all script

--- __compile_all.ps1
+++ __compile_all.ps1
@@ -1,7 +1,12 @@
 Get-ChildItem . -File | ForEach-Object {
-    if($_.Extension -eq ".c") {
+    if ($_.Extension -eq ".c") {
         $Name = $_.BaseName
         Write-Host "Compile $Name ..."
         gcc -o "bin\$Name" "$Name.c"
+        if ($LastExitCode -eq 0) {
+            Write-Host "Compilation succeeded!`n" -ForegroundColor Green
+        } else {
+            Write-Host "Compilation failed with exit code $LastExitCode`n" -ForegroundColor Red
+        }
     }
 }
